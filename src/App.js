0cc64ef1464c27b02081bb4e12b17d658058340c
Senatauro <cgsste@hotmail.com>
2022-06-16 10:55:56 -0300

    v1
    new chalenge from the frontend mentor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import QRGenerator from "./component/QRGenerator";
+
+import qr from "./images/image-qr-code.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="qr-code-holder">
+          <QRGenerator value="https://www.google.com" />
+        </div>
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
     </div>
   );
 }
---
+++ src/component/QRGenerator.js
@@ -0,0 +1,15 @@
+import QRCode from "react-qr-code";
+
+export default function QRGenerator(props) {
+  return (
+    <div>
+      <QRCode
+              value={props.value}
+              size="160"
+              bgColor="#2B7DFA"
+              fgColor="#FFFFFF"
+          />
+        </div>
+    );
+}
+      
